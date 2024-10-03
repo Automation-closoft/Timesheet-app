3689904c3bd36cd0518c1d67f0387d6c44dd8b39
Automation-closoft <info@clo-soft.com>
2024-10-03 17:08:26 +0530

    Update urls.py

--- timesheet_project/urls.py
+++ timesheet_project/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from django.conf import settings
-from django.conf.urls.static import static  # Import for serving media files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('login')),  # Redirect the root URL to the login page
     path('', include('timesheet_app.urls')),  # Include the app's URLs
 ]
-
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
