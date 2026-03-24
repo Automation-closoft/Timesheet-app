882399ca4af0790d0b7bf6737e51266af65c325f
Automation-closoft <info@clo-soft.com>
2026-03-24 17:03:52 +0530

    Update urls.py

--- timesheet_app/urls.py
+++ timesheet_app/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path('success/', success_view, name='success'),
     path('password_change/', password_change_view, name='password_change'),
     path('password_change_done/', password_change_done, name='password_change_done'),
-    path('download-timesheets/', admin_download_timesheets, name='admin_download_timesheets'),  # URL for downloading timesheets
+    path('download-timesheets/', admin_download_timesheets, name='download_timesheets'),  # URL for downloading timesheets
 ]
