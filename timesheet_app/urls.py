f64fd937b817a0666550be2ea50a34f947c43f2c
Automation-closoft <info@clo-soft.com>
2026-03-27 14:42:43 +0530

    Update urls.py

--- timesheet_app/urls.py
+++ timesheet_app/urls.py
@@ -1,22 +1,17 @@
+from django.contrib import admin
 from django.urls import path
-from .views import (
-    signup,
-    login_view,
-    home,
-    logout_view,
-    success_view,
-    password_change_view,
-    password_change_done,
-    admin_download_timesheets,
-)
+from django.shortcuts import redirect
+from timesheet_app import views
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path('login/', login_view, name='login'),
-    path('home/', home, name='home'),
-    path('logout/', logout_view, name='logout'),
-    path('success/', success_view, name='success'),
-    path('password_change/', password_change_view, name='password_change'),
-    path('password_change_done/', password_change_done, name='password_change_done'),
-    path('download-timesheets/', admin_download_timesheets, name='download_timesheets'),  # URL for downloading timesheets
+    path('', lambda request: redirect('login'), name='root'),
+    path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout'),
+    path('success/', views.success_view, name='success'),
+    path('password_change/', views.password_change_view, name='password_change'),
+    path('password_change_done/', views.password_change_done, name='password_change_done'),
+    path('download-timesheets/', views.admin_download_timesheets, name='download_timesheets'),
 ]
