08ccc3fd5d37a838fb0a208773d8e86b667c270f
Automation-closoft <info@clo-soft.com>
2026-03-27 11:03:27 +0530

    Update urls.py

--- timesheet_project/urls.py
+++ timesheet_project/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path
+from timesheet_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('login')),  # Redirect the root URL to the login page
-    path('', include('timesheet_app.urls')),  # Include the app's URLs
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout'),
+    path('success/', views.success_view, name='success'),
+    path('password_change/', views.password_change_view, name='password_change'),
+    path('password_change_done/', views.password_change_done, name='password_change_done'),
+    path('download-timesheets/', views.admin_download_timesheets, name='download_timesheets'),
+    path('timesheets/<str:filename>', views.download_single_timesheet, name='download_single_timesheet'),
 ]
